1939c80655b6986c8d29e41b8b6a4e10b164245c
no-miya <147671633+no-miya@users.noreply.github.com>
2025-06-01 00:12:43 +0000

    Add user following

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -78,8 +78,8 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test "should follow and unfollow a user" do
-    michael  = users(:michael)
-    archer   = users(:archer)
+    michael = users(:michael)
+    archer  = users(:archer)
     assert_not michael.following?(archer)
     michael.follow(archer)
     assert michael.following?(archer)
